5e7b5d2ff302de900a529ccf5d46b99fbda4756f
Daniel Furtado <daniel@dfurtado.com>
2016-04-26 22:49:58 +0200

    lots of changes

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from pyterrier.core import PyTerrier
 
 app = PyTerrier(port=3000)
 
-@app.get("/")
-def action():
-    return app.get_template(name="index.html")
+@app.get("/api/{username:str}")
+def action(username):
+    return app.get_template(name="index2.html", context = {"username": username})
 
 
 if __name__ == "__main__":
