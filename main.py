4dcfa088ef9cca690c2c3d4ae7e1ad900c5a22f9
Daniel Furtado <daniel@dfurtado.com>
2016-05-01 12:00:39 +0200

    added base template

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from pyterrier.core import PyTerrier
 
 app = PyTerrier(port=3000)
 
-@app.get("/api/{username:str}")
-def action(username):
-    return app.get_template(name="index2.html", context = {"username": username})
+@app.get("/")
+def action():
+    return app.get_template(name="index.html")
 
 
 if __name__ == "__main__":
