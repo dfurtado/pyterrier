f565e9b5ed8b46f3c698e409c56972cbcc9c3cd4
Daniel Furtado <daniel@dfurtado.com>
2017-02-01 17:58:21 +0100

    Changed the initial page and create a mock class

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 from pyterrier.core import PyTerrier
-
+from mocks import UserRepository
 app = PyTerrier(port=3000)
 
 @app.get("/")
-def action():
-    return app.view_result(name="index2.html")
-
-
-@app.post('/user/save')
-def save_action(formdata):
-
-    _context = {
-            "firstname": formdata['firstname'],
-            "lastname": formdata['lastname'],
-            "email": formdata['email'],
-    }
-
-    return app.view_result(name="formresults.html", context = _context)
+def index():
+    repo = UserRepository()
+    model = {"users": repo.users}
+    return app.view_result(name="index.html", context=model)
 
 
 if __name__ == "__main__":
     app.run()
 
-
+        
---
+++ mocks/__init__.py
@@ -0,0 +1,1 @@
+from .usersrepo import UserRepository
