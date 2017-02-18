ce352d0bfec1148d1fb2555cdfe8fe87ce108b0c
Daniel Furtado <daniel@dfurtado.com>
2017-02-18 18:52:41 +0100

    Started work adding docstrings

--- pyterrier/http/http_verbs.py
+++ pyterrier/http/http_verbs.py
@@ -1,5 +1,34 @@
 
 def get(route):
+    """
+    Decorator to configure the function as a HTTP GET request
+
+    Usages:
+
+    To create a simple route without any parameters:
+
+    @get("/")
+    def main():
+        return html_result("main.html")
+
+    In the example above the @get decorator states that the function will
+    respond to a HTTP GET request and the return of this function will be the
+    contents of a HTML template, in this case main.htmle
+
+    The @get decorator also accepts variable parameters in the route URL. This
+    can be achieved using placeholders. For example:
+
+    @get("/user/{id:int}")
+    def get_user(id):
+        user = user_repository.get(id)
+        return json_result(user)
+
+    In this case when the client send a HTTP GET request to /users/1, the function
+    get_user will be executed and the id integer parameter will be passed to it.
+
+    The format for the placeholders are {name:type} where type can be: str or int.
+
+    """
     return lambda func: (route, 'GET', func)
 
 def post(route):
