693860aa1c63a13412e9a19c6be439916b0f443a
Daniel Furtado <daniel@dfurtado.com>
2017-04-15 17:33:32 +0200

    Added template app to the used by the CLI

---
+++ pyterrier/app_templates/app.py
@@ -0,0 +1,11 @@
+from pyterrier import PyTerrier
+from pyterrier.http import ViewResult
+
+app = PyTerrier()
+
+@app.get("/")
+def index(self):
+    return ViewResult("index.html", {"message": "App works!"})
+
+if __name__ == "__main__":
+    app.run()
