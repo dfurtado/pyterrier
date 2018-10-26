7897ddc97cc08e9efea9bc0c7d17d6e5126e37d1
Daniel Furtado <danie@dfurtado.com>
2018-10-26 08:06:57 +0200

    Added help texts

--- pyterrier/pyterrier_cli.py
+++ pyterrier/pyterrier_cli.py
@@ -5,9 +5,12 @@ from pyterrier.cli.commands import create_app, create_ctrl
 
 
 @click.command()
-@click.option('--currentdir', is_flag=True)
-@click.option('--newapp')
-@click.option('--newcontroller')
+@click.option('--currentdir', is_flag=True,
+              help='Create the app on the current directory.')
+@click.option('--newapp', metavar='<app name>',
+              help='Name of the new app.')
+@click.option('--newcontroller', metavar='<controller name>',
+              help='Name of the new controller.')
 def main(currentdir, newapp, newcontroller):
     if newapp is not None and newcontroller is not None:
         print(('pyterrier: error: --newapp and --newcontroller are not meant'
