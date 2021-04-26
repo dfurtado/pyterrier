6461c1cc1e835ad4b58c57e92efeaffe9a32dd16
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-04-26 16:26:20 +0100

    fixed typo

--- pyterrier/__init__.py
+++ pyterrier/__init__.py
@@ -1,7 +1,7 @@
 from .http import http_result, view_result, Ok, NotFound, NoContent as http
 from .pyterrier import PyTerrier
 
-def init(*args, **kawrgs):
+def init(*args, **kwargs):
   raise NotImplementedError("You have installed pyterrier the web framework, not PyTerrier the information retrieval toolkit - " +
                             "To install the latter, use 'pip install python-terrier'. For more information see " +
                             "https://github.com/terrier-org/pyterrier/wiki/Installation-of-Wrong-Package")
