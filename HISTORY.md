111f0a7a2be5c88602c3779e1574c586bd1e18ff
Daniel Furtado <daniel@dfurtado.com>
2021-04-26 18:16:59 +0200

    Prepare release 0.1.5

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,10 @@
 # History
 
+
+### 0.1.5 (2021-04-26)
+
+* Added warning message when executing the pyterrier.init()
+
 ### 0.1.4 (2018-10-29)
 
 * Documentation fixes.
--- setup.py
+++ setup.py
@@ -5,60 +5,70 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.md') as history_file:
+with open("HISTORY.md") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'jinja2>=2.10', ]
+requirements = [
+    "Click>=6.0",
+    "jinja2>=2.10",
+]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest', ]
+test_requirements = [
+    "pytest",
+]
 
 setup(
     author="Daniel Furtado",
-    author_email='daniel@dfurtado.com',
+    author_email="daniel@dfurtado.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Environment :: Web Environment',
-        ],
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Environment :: Web Environment",
+    ],
     description="The simplest web framework you will ever use",
     entry_points={
-        'console_scripts': [
-            'pyterrier=pyterrier.pyterrier_cli:main',
+        "console_scripts": [
+            "pyterrier=pyterrier.pyterrier_cli:main",
         ],
     },
     install_requires=requirements,
     license="GNU General Public License v3",
-    long_description=readme + '\n\n' + history,
-    long_description_content_type='text/markdown',
+    long_description=readme + "\n\n" + history,
+    long_description_content_type="text/markdown",
     include_package_data=True,
-    keywords='pyterrier web webdevelopment rest webframework',
-    name='pyterrier',
-    packages=find_packages(include=[
-        'pyterrier',
-        'pyterrier.app_template',
-        'pyterrier.cli',
-        'pyterrier.core',
-        'pyterrier.encoders',
-        'pyterrier.http',
-        'pyterrier.renderers',
-        'pyterrier.validators']),
+    keywords="pyterrier web webdevelopment rest webframework",
+    name="pyterrier",
+    packages=find_packages(
+        include=[
+            "pyterrier",
+            "pyterrier.app_template",
+            "pyterrier.cli",
+            "pyterrier.core",
+            "pyterrier.encoders",
+            "pyterrier.http",
+            "pyterrier.renderers",
+            "pyterrier.validators",
+        ]
+    ),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/dfurtado/pyterrier',
-    version='0.1.4',
+    url="https://github.com/dfurtado/pyterrier",
+    version="0.1.5",
     zip_safe=False,
 )
