808ba50c7f49fdaa0584ccb729839005af8e13ef
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-04-26 16:33:14 +0100

    fixed indentation, line 5.

--- pyterrier/__init__.py
+++ pyterrier/__init__.py
@@ -2,6 +2,6 @@ from .http import http_result, view_result, Ok, NotFound, NoContent as http
 from .pyterrier import PyTerrier
 
 def init(*args, **kwargs):
-  raise NotImplementedError("You have installed pyterrier the web framework, not PyTerrier the information retrieval toolkit - " +
-                            "To install the latter, use 'pip install python-terrier'. For more information see " +
-                            "https://github.com/terrier-org/pyterrier/wiki/Installation-of-Wrong-Package")
+    raise NotImplementedError("You have installed pyterrier the web framework, not PyTerrier the information retrieval toolkit - " +
+                              "To install the latter, use 'pip install python-terrier'. For more information see " +
+                              "https://github.com/terrier-org/pyterrier/wiki/Installation-of-Wrong-Package")
